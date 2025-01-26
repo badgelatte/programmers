48a2134910b83f99c6572ad9e0b0f40d076ed9a3
Lee Jihyeon <saucy5939@gmail.com>
2025-01-26 12:27:37 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.12 ms, Memory: 80.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int max = 0;
+        for(int i = 0; i < numbers.length; i++) {
+            for(int j = 0 ; j < numbers.length; j++) {
+                if(i == j) {
+                    break;
+                }
+                if(max < numbers[i] * numbers[j]) {
+                    max = numbers[i] * numbers[j];
+                }
+            }
+        }
+        return max;
+    }
+}
