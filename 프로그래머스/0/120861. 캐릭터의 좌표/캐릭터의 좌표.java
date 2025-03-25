ea923e922849c69d1716b515bba0261c84ed4913
Lee Jihyeon <saucy5939@gmail.com>
2025-03-25 17:28:06 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.05 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] solution(String[] keyinput, int[] board) {
+        int[] location = {0, 0};
+        
+        for(String key: keyinput) {
+            location = move(key, location, board);
+        }
+        
+        return location;
+    }
+    
+    public int[] move(String key, int[] location, int[] board) {
+        if(key.equals("up") && (location[1] != board[1] / 2)) {
+            location[1]++;
+        }
+        
+        if(key.equals("down") && (location[1] != board[1] / 2 * -1)) {
+            location[1]--;
+        }
+        
+        if(key.equals("left") && (location[0] != board[0] / 2 * -1)) {
+            location[0]--;
+        }
+        
+        if(key.equals("right") && (location[0] != board[0] / 2)) {
+            location[0]++;
+        }
+        
+        return location;
+    }
+}
