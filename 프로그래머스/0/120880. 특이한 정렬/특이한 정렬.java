33c727fee6b2887343666d2c53799a06cb0e81f2
Lee Jihyeon <saucy5939@gmail.com>
2025-03-25 17:40:29 +0900

    [level 0] Title: 특이한 정렬, Time: 1.11 ms, Memory: 78.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = new int[numlist.length];
+        
+        for(int i = 0; i < numlist.length; i++) {
+            numlist[i] -= n;
+        }
+        
+        Arrays.sort(numlist);
+        
+        if(numlist[0] >= 0) {
+            for(int i = 0; i < numlist.length; i++) {
+                numlist[i] += n;
+            }
+            return numlist;
+        }
+        
+        int min = 0;
+        
+        for(int i = 0; i < numlist.length; i++) {
+            if(Math.abs(numlist[i]) < Math.abs(numlist[min])) {
+                min = i;
+            }
+        }
+        
+        answer[0] = numlist[min] + n;
+        int before = min - 1;
+        int after = min + 1;
+        
+        for(int i = 1; i < numlist.length; i++) {
+            if(before < 0) {
+                answer[i] = numlist[after++] + n;
+                continue;
+            }
+            if(after >= numlist.length) {
+                answer[i] = numlist[before--] + n;
+                continue;
+            }
+            
+            if(Math.abs(numlist[before])  < numlist[after]) {
+                answer[i] = numlist[before--] + n;
+            } 
+            else {
+                answer[i] = numlist[after++] + n;
+            }
+        }
+        
+        return answer;
+    }
+}
