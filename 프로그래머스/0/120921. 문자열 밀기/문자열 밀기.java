4c145979b53fa743c20e208b5f924a78baf8bbc2
Lee Jihyeon <saucy5939@gmail.com>
2025-03-27 19:45:45 +0900

    [level 0] Title: 문자열 밀기, Time: 0.25 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int solution(String A, String B) {
+        if(isSame(A,B)) {
+            return 0;
+        }
+        
+        String[] ALetters = A.split("");
+        int count = A.length();
+        
+        while(count-- > 0) {
+            if(B.equals(moveRight(ALetters))) {
+                return A.length() - count;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public boolean isSame(String A, String B) {
+        return A.equals(B);
+    }
+    
+    public String moveRight (String[] letters){
+        String temp = "";
+        
+        for(int i = letters.length - 1; i > 0; i--) {
+            temp = letters[i];
+            letters[i] = letters[i-1];
+            letters[i-1] = temp;
+        }
+        
+        return arrToStr(letters);
+        
+    }
+    
+    public String arrToStr(String[] letters) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(String letter : letters) {
+            sb.append(letter);
+        }
+        
+        return sb.toString();
+    }
+}
