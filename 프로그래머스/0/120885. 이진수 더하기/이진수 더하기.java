1c63a871d5e924c1474978f589bd8a9858d26c25
Lee Jihyeon <saucy5939@gmail.com>
2025-04-03 13:28:35 +0900

    [level 0] Title: 이진수 더하기, Time: 0.08 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120885. 이진수 더하기/이진수 더하기.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String solution(String bin1, String bin2) {
+        int num1 = Integer.parseInt(bin1);
+        int num2 = Integer.parseInt(bin2);
+        long sum = (long)num1 + num2;
+        
+        long[] bins = new long[String.valueOf(sum).length() + 1];
+        int length = bins.length - 1;
+        
+        while(sum > 0) {
+            if(sum % 10 >= 2) {
+                bins[length--] = sum % 10 - 2;
+                sum = sum / 10 + 1;
+            }
+            else {
+                bins[length--] = sum % 10;
+                sum /= 10;
+            }
+        }
+        
+        sum = 0;
+        for(int i = 0; i < bins.length; i++) {
+            sum = sum * 10 + bins[i];
+        }
+        
+        return String.valueOf(sum);
+    }
+}
