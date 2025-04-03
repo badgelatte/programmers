58037bc05d740ab2b155a65093251f163a04bf8e
Lee Jihyeon <saucy5939@gmail.com>
2025-04-03 13:25:01 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.11 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -1,15 +1,32 @@
-import java.util.Arrays;
-
 class Solution {
     public int solution(int[] array, int height) {
         int ranking = array.length;
-        Arrays.sort(array);
+        
+        array = sortArr(array);
+        
         for(int i = 0; i < array.length; i++) {
             if(height < array[i]) {
                 ranking = i;
                 break;
             }
         }
+        
         return array.length - ranking;
+    }
+    
+    public int[] sortArr(int[] arr) {
+        int temp = 0;
+        
+        for(int i = 0; i < arr.length; i++) {
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[i] > arr[j]) {
+                    temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        
+        return arr;
     }
 }
