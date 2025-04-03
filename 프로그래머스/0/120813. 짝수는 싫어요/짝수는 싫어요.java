31d6189be9d46e5c26384f5dc8e1acfa9b0dec7e
Lee Jihyeon <saucy5939@gmail.com>
2025-04-03 13:11:05 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,12 +1,12 @@
 class Solution {
     public int[] solution(int n) {
         int length = n % 2 == 0 ? n/2 : n/2+1;
-        int[] answer = new int[length];
+        int[] array = new int[length];
         int count = 0;
         
         while(++count <= length) {
-            answer[count-1] = 2*count-1;
+            array[count-1] = 2*count-1;
         }
-        return answer;
+        return array;
     }
 }
