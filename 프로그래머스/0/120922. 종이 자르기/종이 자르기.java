87925107bf6b4660623d3115c446b8abff3f200f
Lee Jihyeon <saucy5939@gmail.com>
2025-04-03 13:29:24 +0900

    [level 0] Title: 종이 자르기, Time: 0.02 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
+++ 프로그래머스/0/120922. 종이 자르기/종이 자르기.java
@@ -1,9 +1,6 @@
 class Solution {
     public int solution(int M, int N) {
-        int answer = 0;
-        int min = Math.min(M,N);
-        int max = Math.max(M,N);
         
-        return (min - 1) + (max - 1) * min;
+        return M * N - 1;
     }
 }
