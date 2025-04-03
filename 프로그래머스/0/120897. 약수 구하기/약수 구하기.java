10a8b82c8e3b02a9279b67b425aa4459403efece
Lee Jihyeon <saucy5939@gmail.com>
2025-04-03 13:21:59 +0900

    [level 0] Title: 약수 구하기, Time: 0.30 ms, Memory: 88.9 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.java
@@ -4,11 +4,13 @@ class Solution {
     public int[] solution(int n) {
         int[] answer = new int[n];
         int count = 0;
+        
         for(int i = 1; i <= n; i++) {
             if(n % i == 0) {
                 answer[count++] = i;
             }
         }
+        
         return Arrays.copyOfRange(answer, 0, count);
     }
 }
