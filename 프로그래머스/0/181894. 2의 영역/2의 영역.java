04a2e07c69049a77683f67f11e1bad52b3540470
Lee Jihyeon <saucy5939@gmail.com>
2025-06-01 15:28:09 +0900

    [level 0] Title: 2의 영역, Time: 5.87 ms, Memory: 99.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181894. 2의 영역/2의 영역.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        int start = -1;
+        int end = -1;
+        
+        for(int i = 0; i < arr.length; i++) {
+            // start 지점 저장
+            if(arr[i] == 2 && start < 0) {
+                start = i;
+            }
+            // 2번째 2 지점 저장
+            else if(start >= 0 && arr[i] == 2) {
+                end = i;
+            }
+        }
+
+        if(start != -1) {
+            if(end == -1) {
+                return new int[]{arr[start]};
+                
+            }
+            return arrSub(arr, start, end);
+            
+        }
+
+        return new int[]{-1};
+    }
+    
+    public static int[] arrSub(int[] arr, int start, int end) {
+        int[] result = new int[end-start+1];
+        int now = 0;
+
+        for(int i = start; i <= end; i++) {
+            result[now++] = arr[i];
+        }
+
+        return result;
+    }
+}
