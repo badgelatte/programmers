8ef15701efb252b85f1034bdc48d4764e41dc7f2
Lee Jihyeon <saucy5939@gmail.com>
2026-02-08 21:46:34 +0900

    [level 1] Title: 모의고사, Time: 1.77 ms, Memory: 76.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,59 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        // 수포자 1 = 1,2,3,4,5 반복
+        // 수포자 2 = 2,1,2,3,2,4,2,5 반복
+        // 수포자 3 = 3,3,1,1,2,2,4,4,5,5 반복
+        
+        int[][] testAnswers = {{1, 2, 3, 4, 5}, 
+                          {2, 1, 2, 3, 2, 4, 2, 5}, 
+                          {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+        int[] correctAnswers = new int[3];
+        
+        for(int i = 0; i < correctAnswers.length; i++) {
+            correctAnswers[i] = compareAnswer(answers, testAnswers[i]);
+        }
+
+        return findTopScore(correctAnswers);
+    }
+
+    public int compareAnswer(int[] answers, int[] testAnswer) {
+        int currentIndex = 0;
+        int count = 0;
+
+        for (int answer : answers) {
+            if (answer == testAnswer[currentIndex++]) {
+                count++;
+            }
+
+            if (currentIndex == testAnswer.length) {
+                currentIndex = 0;
+            }
+        }
+
+        return count;
+    }
+
+    public int[] findTopScore(int[] arr) {
+        List<Integer> list = new ArrayList<>();
+        int[] arrSort = arr.clone();
+        Arrays.sort(arrSort);
+
+        for(int i = 0; i < arr.length; i++) {
+            if(arr[i] == arrSort[arrSort.length - 1]) {
+                list.add(i + 1);
+            }
+        }
+
+        int[] topScore = new int[list.size()];
+
+        for(int i = 0; i < list.size(); i++) {
+            topScore[i] = list.get(i);
+        }
+
+        return topScore;
+    }
+}
