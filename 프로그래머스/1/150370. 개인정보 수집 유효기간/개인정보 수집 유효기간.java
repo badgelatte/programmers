ed67037b419cc387fea0ce43045e5523ed4bcdde
Lee Jihyeon <saucy5939@gmail.com>
2026-03-07 05:23:06 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 5.85 ms, Memory: 81.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -0,0 +1,41 @@
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(String today, String[] terms, String[] privacies) {
+        List<Integer> list = new ArrayList<>();
+        LocalDate todayDate = makeLocalDate(today);
+
+        for (int i = 0; i < privacies.length; i++) {
+            String[] privacy = privacies[i].split(" ");
+            LocalDate period = makeLocalDate(privacy[0]);
+
+            for (String term : terms) {
+                String[] termArr = term.split(" ");
+                if (termArr[0].equals(privacy[1])) {
+                    period = period.plusMonths(Integer.parseInt(termArr[1]));
+                }
+            }
+
+            if (todayDate.isAfter(period) || todayDate.isEqual(period)) {
+                list.add(i + 1);
+            }
+        }
+
+        int[] answer = new int[list.size()];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+    public LocalDate makeLocalDate(String date) {
+        String[] dates = date.split("\\.");
+        return LocalDate.of(Integer.parseInt(dates[0]),
+                Integer.parseInt(dates[1]),
+                Integer.parseInt(dates[2]));
+    }
+}
