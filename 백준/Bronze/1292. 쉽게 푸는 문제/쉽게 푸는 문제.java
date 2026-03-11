77cf9eeaba999c66c5b13145025e9f413b22f555
Lee Jihyeon <saucy5939@gmail.com>
2026-03-11 16:48:38 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 88 ms, Memory: 12824 KB -BaekjoonHub

---
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+        int count = 1;
+        int sum = 0;
+
+        for (int i = 1; count <= end; i++) {
+            for ( int j = 0; j < i; j++) {
+                if(count > end) {
+                    break;
+                }
+                if(count++ >= start) {
+                    sum += i;
+                }
+            }
+        }
+
+        System.out.println(sum);
+        
+    }
+}
